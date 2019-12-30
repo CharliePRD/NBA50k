3b288700f5c64ea34441c2d57dd8399771d6ae29
charlieprd <ubuntu@ide.ide.ba5a98ea-3ad4-4ece-8623-384959e98677.svc.cluster.local>
2019-12-30 01:15:21 +0000

    38 commit

--- application.py
+++ application.py
@@ -268,9 +268,11 @@ def register():
             return apology("password and confirm password must match", 403)
         usernames = cur.execute("SELECT username FROM users")
         usernames = cur.fetchall()
+        print("\n\n\n\n username format:")
+        print(usernames)
         #confirms username doesn't exist
         for y in usernames:
-            if username == y["username"]:
+            if username == y:
                 return apology("username already taken", 403)
         #inserts the new user into the users database
         cur.execute("INSERT INTO users (username, hash) VALUES (:username, :hash)", username=username, hash=h_password)
